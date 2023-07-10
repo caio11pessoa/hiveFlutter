9ce0a84e98534740c0f89435f2c5b0bd2a1a1781
Caio Pessoa <caio.pessoa@institutoiracema.org.br>
2023-07-10 13:16:52 -0300

    Criando a interface para a recepção do Hive

--- hiveteste/lib/main.dart
+++ hiveteste/lib/main.dart
@@ -2,20 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hiveteste/bank_account.dart';
 
-late Box box;
+// late Box box;
 Future<void> main() async{
-  await Hive.initFlutter();
-  box = await Hive.openBox('testBox');
-  Hive.registerAdapter(BankAccountAdapter());
-  box.put(
-    'bankAccount', 
-    BankAccount(currency: "\$BR", myMoney: 10000000),
-  );
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // await Hive.initFlutter();
+  // box = await Hive.openBox('testBox');
+  // box = await Hive.openBox('testBox');
+  // Hive.registerAdapter(BankAccountAdapter());
+  // box.put(
+  //   'bankAccount', 
+  //   BankAccount(currency: "\$BR", myMoney: 10000000),
+  // );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -39,12 +42,73 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _quantityController = TextEditingController();
+  
+  void _showForm(BuildContext ctx, int? itemKey) async {
+    showModalBottomSheet(
+      context: ctx, 
+      elevation: 5,
+      isScrollControlled: true,
+      builder: (_) => Container(
+        padding: EdgeInsets.only(
+          bottom: MediaQuery.of(ctx).viewInsets.bottom,
+          top: 15,
+          left: 15,
+          right: 15
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            TextField(
+              controller: _nameController,
+              decoration: const InputDecoration(hintText: 'Name'),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextField(
+              controller: _quantityController,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(hintText: 'Quantity'),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                //Clear the text fields
+                _nameController.text = '';
+                _quantityController.text = '';
+                Navigator.of(context).pop();
+              }, 
+              child: Text('Create New')
+              ),
+              const SizedBox(
+              height: 10,
+              ),
+          ],
+        ),
+      )
+      );
+  }
 
   @override
   Widget build(BuildContext context) {
-    BankAccount bankAccount = box.get('bankAccount');
-    return Scaffold(appBar: AppBar(
-      title: Text("${bankAccount.currency} ${bankAccount.myMoney}"),
-    ),);
+    // BankAccount bankAccount = box.get('bankAccount');
+    // return Scaffold(appBar: AppBar(
+    //   title: Text("${bankAccount.currency} ${bankAccount.myMoney}"),
+    // ),
+    // );
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Hive'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _showForm(context, null),
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }

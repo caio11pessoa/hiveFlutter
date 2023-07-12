6debe4a2be3bdf25bdb5aab4f27adc5ead363534
Caio Pessoa <caio.pessoa@institutoiracema.org.br>
2023-07-12 10:06:03 -0300

    implementação de um crud completo.

--- hiveteste/lib/main.dart
+++ hiveteste/lib/main.dart
@@ -6,8 +6,8 @@ import 'package:hiveteste/bank_account.dart';
 Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
 
-  // await Hive.initFlutter();
-  // box = await Hive.openBox('testBox');
+  await Hive.initFlutter();
+  await Hive.openBox('shopping_box');
   // box = await Hive.openBox('testBox');
   // Hive.registerAdapter(BankAccountAdapter());
   // box.put(
@@ -45,7 +45,58 @@ class _MyHomePageState extends State<MyHomePage> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _quantityController = TextEditingController();
   
+  List<Map<String, dynamic>> _items = [];
+
+  final _shoppingBox = Hive.box('shopping_box');
+
+  void initState() {
+    super.initState();
+    _refreshItems();
+  }
+
+  void _refreshItems(){
+    final data = _shoppingBox.keys.map((key) {
+      final item = _shoppingBox.get(key);
+      return {"key": key, "name": item["name"], "quantity" : item['quantity']};
+    }).toList();
+
+    setState(() {
+      // we use "reversed" to sort items in order from the latest to the oldest
+      _items = data.reversed.toList();
+      print(_items.length);
+    });
+  }
+
+  Future<void> _createItem(Map<String, dynamic> newItem) async {
+    await _shoppingBox.add(newItem);
+    _refreshItems();
+     
+  }
+
+  Future<void> _updateItem(int itemKey, Map<String, dynamic> item) async {
+    await _shoppingBox.put(itemKey, item);
+    _refreshItems();
+  }
+
+  Future<void> _deleteItem(int itemKey) async {
+    await _shoppingBox.delete(itemKey);
+    _refreshItems();
+
+    // Display a snackbar
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text('An item has been deleted'))
+    );
+  }
+
   void _showForm(BuildContext ctx, int? itemKey) async {
+
+if(itemKey != null) {
+  final existingItem = 
+  _items.firstWhere((element) => element['key'] == itemKey);
+  _nameController.text = existingItem['name'];
+  _quantityController.text = existingItem['quantity'];
+}
+
     showModalBottomSheet(
       context: ctx, 
       elevation: 5,
@@ -78,12 +129,27 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             ElevatedButton(
               onPressed: () async {
+
+                if (itemKey == null) {
+                  _createItem({
+                    "name": _nameController.text,
+                    "quantity": _quantityController.text
+                  });
+                }
+
+                if (itemKey != null) {
+                  _updateItem(itemKey, {
+                    'name': _nameController.text.trim(),
+                    'quantity': _quantityController.text.trim()
+                  });
+                }
+
                 //Clear the text fields
                 _nameController.text = '';
                 _quantityController.text = '';
                 Navigator.of(context).pop();
               }, 
-              child: Text('Create New')
+              child: Text(itemKey == null ? 'Create New' : 'Update')
               ),
               const SizedBox(
               height: 10,
@@ -105,6 +171,32 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: const Text('Hive'),
       ),
+      body: ListView.builder(
+        itemCount: _items.length,
+        itemBuilder: (_, index) {
+          final currentItem = _items[index];
+          return Card(
+            color: Colors.orange.shade100,
+            margin: const EdgeInsets.all(10),
+            elevation: 3,
+            child: ListTile(
+              title: Text(currentItem['name']),
+              subtitle: Text(currentItem['quantity'].toString()),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.edit),
+                    onPressed: () => _showForm(context, currentItem['key']), 
+                    ),
+                  IconButton(
+                    icon: const Icon(Icons.delete),
+                    onPressed: () => _deleteItem(currentItem['key'])),
+                ]
+                ),
+            )
+          );
+        },),
       floatingActionButton: FloatingActionButton(
         onPressed: () => _showForm(context, null),
         child: const Icon(Icons.add),
